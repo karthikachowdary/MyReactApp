52d00904952b0ec34ed3c78fe8476404f6067a8b
karthikachowdary <carthika.j@gmail.com>
2026-08-06 20:43:01 +0530

    ci pipeline steps

--- src/index.js
+++ src/index.js
@@ -1,49 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import xyz, {person as p,x} from './App';
-//import xyz from './App';
 import AppC from './App';
 
 /*let h1 = React.createElement('h1',{},"this is h1 tag");
 let root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(h1);*/
 
-let root = ReactDOM.createRoot(document.getElementById('root'));
+ let root = ReactDOM.createRoot(document.getElementById('root'));
 
-let name = 'Karthika';
+// let name = 'Karthika';
 
-let menu =(<ul>
-  <li>{name}</li>
-  <li>Home</li>
-<li>About</li>
-</ul>)
-function Menu(){
-  /*console.log(p);
-  console.log(x);
-  console.log(xyz);
-  return menu;*/
-}
-function Header(){
-  //return (header);
-}
-class Menuclass extends React.Component{
+// let menu =(<ul>
+//   <li>{name}</li>
+//   <li>Home</li>
+// <li>About</li>
+// </ul>)
+// function Menu(){
+//   /*console.log(p);
+//   console.log(x);
+//   console.log(xyz);
+//   return menu;*/
+// }
+// function Header(){
+//   //return (header);
+// }
+// class Menuclass extends React.Component{
 
-  render(){
-    let menus = ['Home','About','Test'];
+//   render(){
+//     let menus = ['Home','About','Test'];
     
-let header = <header><ul>
-  <li>{menus[0]}</li>
-  <li>{menus[1]}</li>
-  <li>{menus[2]}</li>
-  </ul></header>
-    return header;
-  }
-}
+// let header = <header><ul>
+//   <li>{menus[0]}</li>
+//   <li>{menus[1]}</li>
+//   <li>{menus[2]}</li>
+//   </ul></header>
+//     return header;
+//   }
+// }
 //root.render(<Menu></Menu>);
 // root.render(<AppC/>)
 //root.render(<Header></Header>);
 //root.render(<Menuclass/>)
 /* child props*/
+
 root.render(<AppC name='btns'>
   <h1>This is my first application</h1>
 <button style={{margin:10}}>call</button>
